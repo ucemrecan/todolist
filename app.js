696e85c2cf919763e5a84a78f2886974b7c1245d
Umut Cemre <ucemrecan@gmail.com>
2022-12-06 15:53:07 +0300

    update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const _ = require("lodash");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // create database
 // connect mongoose
